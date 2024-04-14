2c287910a29ba87b521d527a79c5978875b01b4f
Daniel Holmes <dan.r.holmes@gmail.com>
2024-04-14 11:54:16 +0930

    Add components, add markup to components

---
+++ src/app/add-wish-form/add-wish-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'add-wish-form',
+  templateUrl: './add-wish-form.component.html',
+  styleUrls: ['./add-wish-form.component.css']
+})
+export class AddWishFormComponent {
+
+}
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-todo'`, () => {
+  it(`should have as title 'wishlist-angular-16'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-todo');
+    expect(app.title).toEqual('wishlist-angular-16');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-todo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('wishlist-angular-16 app is running!');
   });
 });
